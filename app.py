33c078b2ad6aadf0329c69f7ff4eb840e81af161
morreene <morreene@gmail.com>
2019-10-10 18:03:53 +0200

    new

--- app.py
+++ app.py
@@ -18,6 +18,19 @@ app.layout = html.Div([
         value='LA'
     ),
     html.Div(id='display-value')
+        dcc.Graph(
+            id='example-graph',
+            figure={
+                'data': [
+                    {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                ],
+                'layout': {
+                    'title': 'Dash Data Visualization'
+                }
+            }
+        )
+
 ])
 
 @app.callback(dash.dependencies.Output('display-value', 'children'),
